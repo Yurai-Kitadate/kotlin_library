6378b8da15a848ddacfef1e53aafeba60abc696d
tk <82942294+Yurai-Kitadate@users.noreply.github.com>
2021-11-17 01:39:37 +0900

    Create mod_pow.py

---
+++ number/mod_pow.py
@@ -0,0 +1,15 @@
+fun mod_pow(x:Long,n:Long,p: Long): Long{
+    var ans:Long = 1
+    var X: Long = x
+    var N: Long = n
+    while (N >= 1.toLong()){
+        if (N % 2 == 1.toLong()){
+            ans *= X
+            ans = ans % p
+        }
+        X *= X
+        X = X % p
+        N = N / 2
+    }
+    return ans
+}
